1f1053b40703a69aaabe7ac3f1b107ee752262f6
Evangelos Chasanis <vaggelischasanis@gmail.com>
2024-08-08 09:54:01 +0300

    Describe your changes here

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,9 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Sprockets cache configuration
+  config.assets.configure do |env|
+    env.cache = ActiveSupport::Cache.lookup_store(:file_store, 'C:/Users/vagge/OneDrive/Documents/GitHub/TODOApp/tmp/sprockets-cache')
+  end
 end
--- db/migrate/20240807085618_drop_tasks.rb
+++ db/migrate/20240807085618_drop_tasks.rb
@@ -1,9 +1,21 @@
 class DropTasks < ActiveRecord::Migration[7.1]
   def up
-    drop_table :tasks
+    # Check if the table exists before trying to drop it
+    if table_exists?(:tasks)
+      drop_table :tasks
+    else
+      puts "Table 'tasks' does not exist, skipping drop."
+    end
   end
 
   def down
-    raise ActiveRecord::IrreversibleMigration
+    # Define the reverse of this migration if needed
+    create_table :tasks do |t|
+      t.string :name
+      t.boolean :completed, default: false
+      t.integer :user_id
+      t.timestamps
+    end
+    add_index :tasks, :user_id
   end
 end
